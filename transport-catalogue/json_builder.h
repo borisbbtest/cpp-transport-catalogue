73bf441a41b32d2731c5e62ba084480fd3353e13
Boris <boris.z.077@gmail.com>
2023-04-19 01:47:23 +0300

    +

--- transport-catalogue/json_builder.h
+++ transport-catalogue/json_builder.h
@@ -2,101 +2,74 @@
 
 #include "json.h"
 
-// Убрал лишние обертки. Наследование от билдераконтекта  тяжело убрать проще переписать с нуля.
 namespace json
 {
-    class BuildContext;
+
     class KeyContext;
     class StartDictContext;
     class StartArrayContext;
-    class ValueContext;
-    class ValueAfterKeyContext;
-    class ValueAfterStartArrayContext;
 
     class Builder
     {
     public:
         const std::vector<Node *> &GetNodesStack() const;
         KeyContext Key(const std::string &s);
-        ValueContext Value(Node::Value v);
+        Builder &Value(Node::Value v , int is_ = 1 );
         StartDictContext StartDict();
         StartArrayContext StartArray();
         Builder &EndDict();
         Builder &EndArray();
         Node Build();
         virtual ~Builder(){};
-        std::vector<Node *> nodes_stack_ = {nullptr};
-        bool complete = false;
+
     protected:
         Node root_;
+        bool complete = false;
+        std::vector<Node *> nodes_stack_ = {nullptr};
         void CheckComplete();
     };
 
-    class BuildContext : public Builder
+    class KeyContext : public Builder
     {
     public:
-        BuildContext(Builder &b) : b_(b){};
-        KeyContext Key(const std::string &s);
-        BuildContext Value(Node::Value v);
-        Node Build();
-        Builder &GetB();
+        KeyContext(Builder &b) : b_(b) {}
+        StartDictContext Value(Node::Value v);
+        KeyContext Key(const std::string &s) = delete;
+        Node Build() = delete;
+        Builder &EndDict() = delete;
+        Builder &EndArray() = delete;
 
-    protected:
+    private:
         Builder &b_;
     };
 
-    class KeyContext : public BuildContext
+    class StartDictContext : public Builder
     {
     public:
-        KeyContext(Builder &b) : BuildContext(b) {}
-        ValueAfterKeyContext Value(Node::Value v);
-        KeyContext Key(const std::string &s) = delete;
-        Node Build() = delete;
-    };
-
-    class StartDictContext : public BuildContext
-    {
-    public:
-        StartDictContext(Builder &b) : BuildContext(b) {}
-        BuildContext Value(Node::Value v) = delete;
+        StartDictContext(Builder &b) : b_(b) {}
+        Builder Value(Node::Value v) = delete;
+        KeyContext Key(const std::string &s);
         StartDictContext StartDict() = delete;
         StartArrayContext StartArray() = delete;
+        Builder &EndArray() = delete;
+        Builder &EndDict();
         Node Build() = delete;
+
+    private:
+        Builder &b_;
     };
 
-    class StartArrayContext : public BuildContext
+    class StartArrayContext : public Builder
     {
     public:
-        StartArrayContext(Builder &b) : BuildContext(b) {}
-        ValueAfterStartArrayContext Value(Node::Value v);
+        StartArrayContext(Builder &b) : b_(b) {}
+        StartArrayContext Value(Node::Value v);
         KeyContext Key(const std::string &s) = delete;
         Node Build() = delete;
-    };
+        Builder &EndDict() = delete;
+        Builder &EndArray();
 
-    class ValueContext : public BuildContext
-    {
-    public:
-        ValueContext(Builder &b) : BuildContext(b) {}
-        KeyContext Key(const std::string &s) = delete;
-        BuildContext Value(Node::Value v) = delete;
+    private:
+        Builder &b_;
     };
-
-    class ValueAfterKeyContext : public BuildContext
-    {
-    public:
-        ValueAfterKeyContext(Builder &b) : BuildContext(b) {}
-        BuildContext Value(Node::Value v) = delete;
-        Builder EndArray() = delete;
-        Node Build() = delete;
-    };
-
-    class ValueAfterStartArrayContext : public BuildContext
-    {
-    public:
-        ValueAfterStartArrayContext(Builder &b) : BuildContext(b) {}
-        BuildContext Value(Node::Value v);
-        KeyContext Key(const std::string &s) = delete;
-        Node Build() = delete;
-    };
-
 }
